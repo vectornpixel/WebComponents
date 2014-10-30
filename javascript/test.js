c5641b7c2eca916fa190dc16e6c8f4cb189479d9
Asim Craft <vectornpixel@gmail.com>
2014-10-30 15:08:47 -0400

    supdates

---
+++ javascript/test.js
@@ -0,0 +1,23 @@
+//*********IF STATEMENTS************//
+if ( i > 20){
+    alert("its more");
+}else{
+    alert("its less");
+}
+
+//check if you can enter
+var access = "access granted";
+var userPass = prompt("whats ur pass");
+if (userPass == "ice"){
+    alert(access);
+}else {
+    alert("no access");
+}
+//*********WHILE LOOP************//
+var image = "display";
+var columns = 10;
+
+while (columns < 15){
+    alert(image);
+}
+
